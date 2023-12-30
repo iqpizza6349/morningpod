ef725ef7705dcfc870705e74a98a5e30ee3bebd7
iqpizza6349 <iqpizza6349@gmail.com>
2023-12-30 20:24:10 +0900

    :lipstick: :: add week widget to show enable/disable day of week

---
+++ lib/component/week_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+/// 이 클래스는 한 주의 요일마다 알람이
+/// 활성되어있는 지 비활성화되어 있는 지를 표현하는
+/// 텍스트 형태의 라벨입니다.
+class WeekWidget extends StatelessWidget {
+  final List<bool> enables;
+
+  const WeekWidget(this.enables, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        text: TextSpan(
+          children: [
+            for (int i = 0; i < 7; i++)
+              TextSpan(
+                text: "${["S  ", "M  ", "T  ", "W  ", "T  ", "F  ", "S"][i]}${i < 6 ? "  " : ""}",
+                style: TextStyle(
+                  color: isDayEnabled(i) ? Colors.white : const Color(0xFF767676),
+                  fontSize: 18,
+                  fontFamily: "Pretendard",
+                  fontWeight: FontWeight.w500,
+                  height: 0
+                )
+              )
+          ]
+        ),
+    );
+  }
+
+  bool isDayEnabled(int dayIndex) {
+    return enables[dayIndex];
+  }
+}
+
+
