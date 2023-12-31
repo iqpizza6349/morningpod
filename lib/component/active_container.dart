b7c4730ab62b5ba470d58f53470097966b88fff5
iqpizza6349 <iqpizza6349@gmail.com>
2023-12-31 19:42:50 +0900

    :lipstick: :: update switch button to library component

--- lib/component/active_container.dart
+++ lib/component/active_container.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 /// 이 클래스는 알람 활성화 버튼이 들어갈 컨테이너 클래스입니다.
 class ActiveSwitch extends StatefulWidget {
@@ -17,10 +18,15 @@ class _ActiveSwitchState extends State<ActiveSwitch> {
     return SizedBox(
       width: 98,
       height: 42,
-      child: CupertinoSwitch(
+      child: FlutterSwitch(
+          width: 98,
+          height: 42,
+          padding: 4,
+          toggleSize: 36,
+          borderRadius: 40,
           value: _isChecked,
           activeColor: const Color(0xFF76EE58),
-          onChanged: (bool? value) {
+          onToggle: (bool? value) {
             setState(() {
               _isChecked = value ?? true;
             });
